4b4db154915b33683d54ee50ecaaddcbf07a3b15
saieswarkumar <saisadam770@gmail.com>
2023-12-02 13:47:43 +0530

    First commit

---
+++ wegodigitally/html/app.js
@@ -0,0 +1,56 @@
+
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+loadContent("wegodigitally.html")
+
+let homeNav = document.getElementById("home");
+homeNav.addEventListener("click",function(e){
+    let page_name = e.target.name;
+    loadContent(page_name);
+})
+
+let aboutNav = document.getElementById("about");
+aboutNav.addEventListener("click",function(e){
+    let page_name = e.target.name;
+    loadContent(page_name);
+})
+
+let DigitalMarketingNav =document.getElementById("DigitalMarketing");
+DigitalMarketingNav.addEventListener("click",function(e){
+    let page_name = e.target.name;
+    loadContent(page_name);
+})
+
+let developerNav =document.getElementById("webDevelopment");
+developerNav.addEventListener("click",function(e){
+    let page_name = e.target.name;
+    loadContent(page_name);
+})
+
+let brandingNav =document.getElementById("Branding");
+brandingNav.addEventListener("click",function(e){
+    let page_name = e.target.name;
+    loadContent(page_name);
+})
+
+let graphicsNav =document.getElementById("graphicsAndDesigning");
+graphicsNav.addEventListener("click",function(e){
+    let page_name = e.target.name;
+    loadContent(page_name);
+})
+let bussinessNav =document.getElementById("bussinessEnquiry");
+bussinessNav.addEventListener("click",function(e){
+    let page_name = e.target.name;
+    loadContent(page_name);
+})
+let carriersNav =document.getElementById("carriersPage");
+carriersNav.addEventListener("click",function(e){
+    let page_name = e.target.name;
+    loadContent(page_name);
+})
